1741c39d3e8d73d0ca87ca59b5e2d3c2d138648a
GDivittorio <gm.divittorio@gmail.com>
2016-12-27 10:30:39 +0100

    Tags fetching query added

--- selfAnswers.sql
+++ selfAnswers.sql
@@ -6,3 +6,9 @@ AND (Questions.CreationDate BETWEEN '2016-11-14'AND '2016-12-14');
 // Query 2 - To count the overall number of questions in the last mont:
 SELECT COUNT(Questions.Id) FROM Posts AS Questions WHERE 
 Questions.CreationDate BETWEEN '2016-11-14'AND '2016-12-14';
+
+// Query 3 - To get a subsample of questions marked with a single tag, alongside the Question ID used to merge the generated csv with the
+// original one
+SELECT Id, Tags FROM Posts WHERE Tags = '<java>' OR Tags = '<javascript>' OR Tags = '<php>' OR Tags = '<python>' OR Tags = '<c#>' 
+INTO OUTFILE '/tmp/topicsStackOF.csv' FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+ 
